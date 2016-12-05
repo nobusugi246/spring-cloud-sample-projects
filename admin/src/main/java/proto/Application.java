584b83a5a9a8d0673bfb442ba42671c73e096e3d
nobusugi246 <nobusugi246@gmail.com>
2016-12-06 01:19:00 +0900

    update configurations.

--- admin/src/main/java/proto/Application.java
+++ admin/src/main/java/proto/Application.java
@@ -3,9 +3,11 @@ package proto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+@EnableDiscoveryClient
+@EnableAdminServer
 @SpringBootApplication
 public class Application {
 
