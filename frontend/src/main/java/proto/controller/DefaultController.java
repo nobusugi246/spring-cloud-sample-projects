90839953c29e9922ed3bb049ab94fed2fec87c70
nobusugi246 <nobusugi246@gmail.com>
2016-12-08 02:34:12 +0900

    add default controller.

---
+++ frontend/src/main/java/proto/controller/DefaultController.java
@@ -0,0 +1,15 @@
+package proto.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class DefaultController {
+    @RequestMapping("/index")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+}
