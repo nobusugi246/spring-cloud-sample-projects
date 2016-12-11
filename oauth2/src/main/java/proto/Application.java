51bd53d762eceaffd37423997e33621cb5848208
nobusugi246 <nobusugi246@gmail.com>
2016-12-11 20:50:11 +0900

    add OAuth2 Microservice.

--- oauth2/src/main/java/proto/Application.java
+++ oauth2/src/main/java/proto/Application.java
@@ -1,13 +1,15 @@
 package proto;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-@EnableDiscoveryClient
-@EnableAdminServer
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableAuthorizationServer
+@EnableResourceServer
 public class Application {
 
 	public static void main(String[] args) {
